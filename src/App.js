3fe7c4d4e5a4823e4eabde34e38aff7642ad9f6f
MaheshSurepalli <surepallimahesh8@gmail.com>
2022-04-01 10:49:25 +0530

    Task Component completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
+import Tasks from './components/Tasks';
+import {useState} from 'react';
 
 function App() {
+  const [tasks,setTasks] = useState([
+    {
+     id : 1,
+     text : 'Book Reading',
+     day : 'April 1st 2022'
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Task Tracker"></Header>
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+
+  const onClick = () => {
+    console.log("Click");
+  }
+  return (
+    <div className="header">
+        <h1>{title}</h1>
+        <Button onClick={onClick} color="green" text='Hello'></Button>
+    </div>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Traker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
